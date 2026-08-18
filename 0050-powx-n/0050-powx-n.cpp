90fe833008d6ef56f6b9284aea9675fa2e8fcfc5
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-19 00:03:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    double power(double x,long long n){
+        if(n==0) return 1.0;
+        double half=power(x,n/2);
+        if(n%2 == 0){
+            return half*half;
+        }
+        return x*half*half;
+    }
+
+    double myPow(double x, int n) {
+        long long num=n;
+        if(num<0){
+            x=1/x;
+            num=-num;
+        }
+        return power(x,num);
+    }
+};
