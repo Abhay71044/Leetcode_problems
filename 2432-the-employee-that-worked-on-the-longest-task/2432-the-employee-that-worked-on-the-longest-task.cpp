3ae7c137299d07ab16f07c6de14ae7c25a4f4c32
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-21 16:10:07 +0530

    Time: 0 ms (100%), Space: 39.4 MB (99.52%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int maxi = logs[0][1];
+        int ans = logs[0][0];
+        for(int i=1;i<logs.size();i++){
+            if(maxi<(logs[i][1]-logs[i-1][1])){
+                ans=logs[i][0];
+                maxi=logs[i][1]-logs[i-1][1];
+            }
+            else if(maxi==(logs[i][1]-logs[i-1][1])){
+                ans = min(ans, logs[i][0]);
+            }
+        }
+        return ans;
+    }
+};
