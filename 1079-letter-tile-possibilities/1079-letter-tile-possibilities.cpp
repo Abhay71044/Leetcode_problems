247cac8904a681da64ca5e248e5006eba9038023
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-24 18:54:10 +0530

    Time: 75 ms (11.46%), Space: 17.1 MB (24.7%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
 
-    void solve(string tiles, set<string>& st, string output) {
-        if (!output.empty()) {
+    void solve(string&tiles,set<string>&st, vector<bool>&visited,string&output){
+        if(!output.empty()){
             st.insert(output);
         }
-        for (int i = 0; i < tiles.size(); i++) {
-            char ch = tiles[i];
-            string remaining = tiles.substr(0, i) + tiles.substr(i + 1);
-            solve(remaining, st, output + ch);
+        for(int i=0;i<tiles.size();i++){
+            if(visited[i]){
+                continue;
+            }
+            visited[i]=true;
+            output.push_back(tiles[i]);
+            solve(tiles,st,visited,output);
+            output.pop_back();
+            visited[i]=false;
         }
     }
 
     int numTilePossibilities(string tiles) {
-        set<string> st;
-        solve(tiles, st, "");
+        set<string>st;
+        vector<bool>visited(tiles.size(),false);
+        string output="";
+        solve(tiles,st,visited,output);
         return st.size();
     }
 };
