a114fd3a1d52f3de888db54eed1c2bb481583efc
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-27 17:11:34 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.89%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum=0;
+        if(k<=numOnes){
+            return k;
+        }
+        else{
+            if(k<=(numOnes+numZeros)){
+                return numOnes;
+            }
+            else{
+                int neg=k-(numOnes+numZeros);
+                sum=numOnes-neg;
+            }
+        }
+        return sum;
+    }
+};
