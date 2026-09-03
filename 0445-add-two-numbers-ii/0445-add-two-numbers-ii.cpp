0b7b795117eeb7ae58e59278a3265e21d4882b0e
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-03 05:47:03 +0530

    Time: 0 ms (100%), Space: 76.9 MB (84.95%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* &head){
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* forward=curr->next;
+        while(curr!=NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        l1=reverse(l1);
+        l2=reverse(l2);
+        int carry =0;
+
+        ListNode* anshead=NULL;
+        ListNode* ansTail=NULL;
+
+        while(l1!=NULL && l2!=NULL){
+            int sum=l1->val+l2->val + carry;
+            int digit=sum%10;
+            carry=sum/10;
+
+            ListNode* newNode=new ListNode(digit);
+            if(anshead==NULL){
+                anshead=newNode;
+                ansTail=newNode;
+            }
+            else{
+                ansTail->next=newNode;
+                ansTail=newNode;
+            }
+            l1=l1->next;
+            l2=l2->next;
+        }
+        while(l1!=NULL){
+            int sum=l1->val + carry;
+            int digit=sum%10;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(digit);
+            ansTail->next=newNode;
+            ansTail=newNode;
+            l1=l1->next;
+        }
+        while(l2!=NULL){
+            int sum=l2->val + carry;
+            int digit=sum%10;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(digit);
+            ansTail->next=newNode;
+            ansTail=newNode;
+            l2=l2->next;
+        }
+        if(carry!=0){
+            int sum= carry;
+            int digit=sum%10;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(digit);
+            ansTail->next=newNode;
+            ansTail=newNode;
+        }
+        return reverse(anshead);
+    }
+};
