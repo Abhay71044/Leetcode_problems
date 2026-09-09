0b2472b7ae4baf557e5a2ba0df54ebcff4e01fc5
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-09 18:08:15 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.94%) - LeetHub

---
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        int ans = purchaseAmount / 10;
+        if(purchaseAmount % 10 >= 5)
+            ans++; 
+        int a = ans * 10;
+        return 100 - a;
+    }
+};
