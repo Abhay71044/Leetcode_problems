1cde0015fae9441403f8c8385d25dd1c0f537503
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-15 19:26:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (49.12%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    vector<int> generateRow(int row){
+        long long ans=1;
+        vector<int>ansRow;
+        ansRow.push_back(1);
+        for(int col=1;col<row;col++){
+            ans=ans*(row-col);
+            ans=ans/(col);
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+
+    vector<int> getRow(int rowIndex) {
+        return generateRow(rowIndex+1);
+    }
+};
+
