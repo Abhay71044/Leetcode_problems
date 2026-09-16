0c161f34cc6df2493ead06dba9f122f631f73b07
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-16 23:17:17 +0530

    Time: 15 ms (30.51%), Space: 34.9 MB (88%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
 
-    int numberOfDays(vector<int>&arr,int d){
-        int sum=0;
-        int count=1;
-        for(int i=0;i<arr.size();i++){
-            if(sum+arr[i]>d){
-                count++;
-                sum=arr[i];
+    int maxele(vector<int>& weights){
+        int maxi=-1;
+        for(int i=0;i<weights.size();i++){
+            maxi=max(weights[i],maxi);
+        }
+        return maxi;
+    }
+
+    int capacity(vector<int>& weights,int cap){
+        int day=1;
+        int load=0;
+        for(int i=0;i<weights.size();i++){
+            if(load+weights[i]>cap){
+                day=day+1;
+                load=weights[i];
             }
             else{
-                sum+=arr[i];
+                load+=weights[i];
             }
         }
-        return count;
+        return day;
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
-        int low=INT_MIN;
+        int low=maxele(weights);
         int high=0;
         for(int i=0;i<weights.size();i++){
-            low=max(low,weights[i]);
-            high=high+weights[i];
+            high+=weights[i];
         }
         int ans=-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(numberOfDays(weights,mid) <= days){
+            int mid=(low+high)/2;
+            if(capacity(weights,mid)<=days){
                 ans=mid;
                 high=mid-1;
             }
