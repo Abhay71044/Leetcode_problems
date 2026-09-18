3d12f83381c6a97ae6dc850d333bf43ef53acb96
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-18 22:17:27 +0530

    Time: 12 ms (17.05%), Space: 13.4 MB (37.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<char,int>mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int ans=0;
         for(int i=0;i<s.length();i++){
-            if(i+1<s.length() && mp[s[i]]<mp[s[i+1]]){
+            if((i+1)<s.length() && mp[s[i]]<mp[s[i+1]]){
                 ans-=mp[s[i]];
             }
             else{
